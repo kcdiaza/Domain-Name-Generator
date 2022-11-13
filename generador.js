8e3840a5070e505c8ba1efc6ad4a55d9bda198af
gabriel7493 <gabriel.diaz@atomixwap.io>
2022-11-12 21:26:01 -0300

    primer commit

---
+++ generador.js
@@ -0,0 +1,11 @@
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon'];
+
+for(let p=0 ; p<pronoun.length ; p++){
+    for(let a=0 ; a<adj.length ; a++){
+        for(let n=0 ; n<noun.length ; n++){
+            console.log(pronoun[p] + adj[a] + noun[n] + ".com")
+        }
+    }  
+}
